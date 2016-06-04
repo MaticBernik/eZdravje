a3116e3f11186cafdfa633d1c9cd17163538578e
Matic  Bernik <mb2551@student.uni-lj.si>
2016-06-04 00:05:31 +0000

    Dodana metoda za generiranje podatkov

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -6,6 +6,55 @@ var username = "ois.seminar";
 var password = "ois4fri";
 
 
+
+function dodajMeritveVitalnihZnakov(ehrId,datumInUra,telesnaVisina,telesnaTeza,telesnaTemperatura,sistolicniKrvniTlak,diastolicniKrvniTlak,nasicenostKrviSKisikom,merilec) {
+	sessionId = getSessionId();
+
+	if (!ehrId || ehrId.trim().length == 0) {
+		$("#dodajMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo " +
+      "label label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+	} else {
+		$.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		var podatki = {
+			// Struktura predloge je na voljo na naslednjem spletnem naslovu:
+      // https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+		    "ctx/language": "en",
+		    "ctx/territory": "SI",
+		    "ctx/time": datumInUra,
+		    "vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+		    "vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+		   	"vital_signs/body_temperature/any_event/temperature|magnitude": telesnaTemperatura,
+		    "vital_signs/body_temperature/any_event/temperature|unit": "°C",
+		    "vital_signs/blood_pressure/any_event/systolic": sistolicniKrvniTlak,
+		    "vital_signs/blood_pressure/any_event/diastolic": diastolicniKrvniTlak,
+		    "vital_signs/indirect_oximetry:0/spo2|numerator": nasicenostKrviSKisikom
+		};
+		var parametriZahteve = {
+		    ehrId: ehrId,
+		    templateId: 'Vital Signs',
+		    format: 'FLAT',
+		    committer: merilec
+		};
+		$.ajax({
+		    url: baseUrl + "/composition?" + $.param(parametriZahteve),
+		    type: 'POST',
+		    contentType: 'application/json',
+		    data: JSON.stringify(podatki),
+		    success: function (res) {
+		        $("#dodajMeritveVitalnihZnakovSporocilo").html(
+              "<span class='obvestilo label label-success fade-in'>" +
+              res.meta.href + ".</span>");
+		    },
+		    error: function(err) {
+		    	$("#dodajMeritveVitalnihZnakovSporocilo").html(
+            "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+            JSON.parse(err.responseText).userMessage + "'!");
+		    }
+		});
+	}
+}
 /**
  * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
  * enolične ID številke za dostop do funkcionalnosti
@@ -31,10 +80,82 @@ function getSessionId() {
  * @return ehrId generiranega pacienta
  */
 function generirajPodatke(stPacienta) {
-  ehrId = "";
+  var sessionId = getSessionId();    
+  var ehrId = "";
 
   // TODO: Potrebno implementirati
+  var ime="";
+  var priimek="";
+  var datumRojstva="";
 
+  
+  if(stPacienta==1){
+    ime="Robert";
+    priimek="Slabsa";
+    datumRojstva="1980-03-10T11:03";  
+  }else if(stPacienta==2){
+    ime="Ana";
+    priimek="Optimum";
+    datumRojstva="1990-06-10T09:03"; 
+  }else if(stPacienta==3){
+    ime="David";
+    priimek="Kriticno";
+    datumRojstva="1951-02-06T03:13";   
+  }else{
+      console.log("Metoda generirajPodatke je bila pripravljena za do 3 paciente!");
+  }
+  
+  $.ajaxSetup({
+		    headers: {"Ehr-Session": sessionId}
+		});
+		$.ajax({
+		    url: baseUrl + "/ehr",
+		    type: 'POST',
+		    success: function (data) {
+		        ehrId = data.ehrId;
+		        var partyData = {
+		            firstNames: ime,
+		            lastNames: priimek,
+		            dateOfBirth: datumRojstva,
+		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+		        };
+		        $.ajax({
+		            url: baseUrl + "/demographics/party",
+		            type: 'POST',
+		            contentType: 'application/json',
+		            data: JSON.stringify(partyData),
+		            success: function (party) {
+		                if (party.action == 'CREATE') {
+		                    $("#kreirajSporocilo").html("<span class='obvestilo " +
+                          "label label-success fade-in'>Uspešno kreiran EHR '" +
+                          ehrId + "'.</span>");
+		                    $("#preberiEHRid").val(ehrId);
+		                }
+		            },
+		            error: function(err) {
+		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                    "label-danger fade-in'>Napaka '" +
+                    JSON.parse(err.responseText).userMessage + "'!");
+		            }
+		        });
+		        
+		        if(stPacienta==1){
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-05-29T07:30","169","67","37.1","128","82","98",ime+" "+priimek);
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-01T07:30","169","68","37.0","129","83","97",ime+" "+priimek);
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-04T07:30","169","71","37.1","130","84","96",ime+" "+priimek);
+		        }else if(stPacienta==2){
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-04T07:30","180","70","37.0","115","70","99",ime+" "+priimek);
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-03T07:30","180","70","37.0","115","70","99",ime+" "+priimek);
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-02T07:30","180","70","37.0","115","70","99",ime+" "+priimek);
+		            dodajMeritveVitalnihZnakov(ehrId,"2016-06-01T07:30","180","70","37.0","115","70","99",ime+" "+priimek);  
+		        }else if(stPacienta==3){
+		           dodajMeritveVitalnihZnakov(ehrId,"2016-06-04T07:30","171","53","39.7","141","91","94","Alojzija Kriticno");
+		           dodajMeritveVitalnihZnakov(ehrId,"2016-05-04T07:30","171","58","39.7","141","91","94","Alojzija Kriticno");
+		        }      
+		    }
+		});
+    
+    
   return ehrId;
 }
 
